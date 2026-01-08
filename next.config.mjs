3eaccdbf366b2877ba9c5e97d4602bbc8e502049
MuzammilAhmed2001 <mamasmobile2021@gmail.com>
2026-01-08 18:55:31 +0500

    created blog with strapi

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,20 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
+    ],
+    unoptimized: true,
+  },
+  sassOptions: {
+    additionalData: `$var: red;`,
+  },
 };
 
 export default nextConfig;
---
+++ src/app/data/actions.ts
@@ -0,0 +1,56 @@
+"use server";
+import { z } from "zod";
+import { subscribeService } from "./services";
+
+const subscribeSchema = z.object({
+  email: z.email({
+    message: "Please enter a valid email address",
+  }),
+});
+
+export async function subscribeAction(prevState: any, formData: FormData) {
+  console.log("Our first server action");
+  const email = formData.get("email");
+  const validatedFields = subscribeSchema.safeParse({
+    email: formData.get("email"),
+  });
+
+ if (!validatedFields.success) {    
+    return {
+      ...prevState,
+      zodErrors: validatedFields.error.flatten().fieldErrors,
+      strapiErrors: null,
+    };
+  }
+
+  const responseData = await subscribeService(validatedFields.data.email);
+
+  if (!responseData) {
+    return {
+      ...prevState,
+      strapiErrors: null,
+      zodErrors: null,
+      errorMessage: "Ops! Something went wrong. Please try again.",
+    };
+  }
+
+  if (responseData.error) {
+    console.log(responseData.error, "from action")
+    return {
+      ...prevState,
+      strapiErrors: responseData.error,
+      zodErrors: null,
+      errorMessage: "Failed to Subscribe.",
+    };
+  }
+  console.log(email, "Our email input from form")
+
+  return {
+    ...prevState,
+    zodErrors: null,
+    strapiErrors: null,
+    errorMessage: null,
+    successMessage: "Successfully Subscribed!",
+  };
+
+}
